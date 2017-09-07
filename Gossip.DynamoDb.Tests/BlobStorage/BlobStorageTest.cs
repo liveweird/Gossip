039df97c6df7b029d1ecb48459abbf6bd2d43845
Sebastian Gebski <sgebski@ecovadis.com>
2017-09-07 11:18:26 +0200

    Empty external service test project

---
+++ Gossip.DynamoDb.Tests/BlobStorage/BlobStorageTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Gossip.DynamoDb.Tests.BlobStorage
+{
+    public class BlobStorageTest
+    {
+        [Fact]
+        public void DoSomethingTest()
+        {
+        }
+    }
+}
